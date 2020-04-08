1065d2b904e7516329c0f4ffe1b12124fab79928
Sarah-DeMatteis <816154@mystma.org>
2020-04-08 18:54:00 +0000

    Added top five favorite movies

---
+++ Unit1/src/app/App.java
@@ -0,0 +1,11 @@
+package app;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("1. Star Wars Original Trilogy");
+        System.out.println("2. The Martian");
+        System.out.println("3. Star Wars Prequel Trilogy");
+        System.out.println("4. Avatar");
+        System.out.println("5. Avengers: Endgame");
+    }
+}
