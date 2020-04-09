438bb74494e54718cf0e2b78a9047db84d3574d8
Sarah-DeMatteis <816154@mystma.org>
2020-04-09 16:38:57 +0000

    Added lots of variables.

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -2,10 +2,44 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("1. Star Wars Original Trilogy");
-        System.out.println("2. The Martian");
-        System.out.println("3. Star Wars Prequel Trilogy");
-        System.out.println("4. Avatar");
-        System.out.println("5. Avengers: Endgame");
+        
+       short favNumber = 571;
+       char stateNameFirstLetter = 'M';
+       boolean isHomeworkDone = false;
+       byte daysInApril = 30;
+       float outsideTemp = 42.3f;
+       double mathAnswer = 34.52647438;
+       int coronavirusCases = 1500000;
+       long starsInMilkyWay = 100000000000L;
+
+
+       System.out.println("My favorite number is:");
+       System.out.println(favNumber);
+
+       System.out.println("True or false: Sarah finished her homework.");
+       System.out.println(isHomeworkDone);
+
+       System.out.println("The first letter of Minnesota is:");
+       System.out.println(stateNameFirstLetter);
+
+       System.out.println("There are");
+       System.out.println(daysInApril);
+       System.out.println("days in April.");
+
+       System.out.println("The answer to math problem 1 is:");
+       System.out.println(mathAnswer);
+
+       System.out.println("The temperature outside is");
+       System.out.println(outsideTemp);
+       System.out.println("degrees Fahrenheit.");
+
+       System.out.println("There are at least");
+       System.out.println(coronavirusCases);
+       System.out.println("coronavirus cases. Probably more.");
+
+       System.out.println("There are around");
+       System.out.println(starsInMilkyWay);
+       System.out.println("stars in the Milky Way Galaxy.");
+
     }
 }
