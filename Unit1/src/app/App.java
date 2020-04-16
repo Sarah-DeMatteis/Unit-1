258452c76567309abc469c0b3ab0fd210698f2e3
Sarah-DeMatteis <816154@mystma.org>
2020-04-16 16:46:16 +0000

    For loops, just harder

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -3,30 +3,27 @@ package app;
 public class App {
     public static void main(String[] args) throws Exception {
 
-        for(int i1 = 10; i1 <= 20; i1 = i1 + 1){
-            System.out.println("Count 1 reads " + i1);
+        for(int i1 = 22; i1 <= 28; i1 = i1 + 3){
+            System.out.println(i1);
         }
-        System.out.println("20 reached. Count 1 complete.");
 
-        for(int i2 = 10; i2 <= 100; i2 = i2 + 1){
-            System.out.println("Count 2 reads " + i2);
+        for(int i2 = 2; i2 <= 32; i2 = i2 * 2){
+            System.out.println(i2);
         }
-        System.out.println("100 reached. Count 2 complete.");
 
-        for(int i3 = 1000; i3 <= 256000; i3 = i3 + 1000){
-            System.out.println("Count 3 reads " + i3);
+        for(int i3 = 3; i3 >= -81; i3 = i3 * -i3){
+            System.out.println(i3);
         }
-        System.out.println("256000 reached. Count 3 complete.");
-
-        for(int i4 = 11; i4 <= 12; i4 = i4 + 1){
-            System.out.println("Count 4 reads " + i4);
+ 
+        for(int i4 = 50; i4 >= 5; i4 = i4 - 5){
+            System.out.println(i4);
         }
-        System.out.println("12 reached. Count 4 complete.");
-
-        for(int i5 = 10; i5 >= 0; i5 = i5 - 1){
-            System.out.println("Count 5 reads " + i5);
+    
+        for(int i5 = 1; i5 <= 9; i5 = i5 + 1){
+            System.out.println(i5 * i5);
+            System.out.println(i5 + 3);
+            System.out.println(i5);
         }
-        System.out.println("0 reached. Final countdown complete.");
 
     }
 }
