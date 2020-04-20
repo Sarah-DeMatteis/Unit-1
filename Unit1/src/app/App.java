404012c94ed3610083c2f80b2ced68c0564394a2
Sarah-DeMatteis <816154@mystma.org>
2020-04-20 15:14:59 +0000

    Triple logic: If, for, and else

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -3,27 +3,15 @@ package app;
 public class App {
     public static void main(String[] args) throws Exception {
 
-        for(int i1 = 22; i1 <= 28; i1 = i1 + 3){
-            System.out.println(i1);
+        for (int i1 = 1; i1 <= 20; i1++) {
+
+            if (i1 % 4 == 0) {
+                System.out.println(i1 + " is divisible by 4.");
+            } else {
+                System.out.println(i1 + " is not divisible by 4.");
+            }
         }
 
-        for(int i2 = 2; i2 <= 32; i2 = i2 * 2){
-            System.out.println(i2);
-        }
-
-        for(int i3 = 3; i3 >= -81; i3 = i3 * -i3){
-            System.out.println(i3);
-        }
- 
-        for(int i4 = 50; i4 >= 5; i4 = i4 - 5){
-            System.out.println(i4);
-        }
-    
-        for(int i5 = 1; i5 <= 9; i5 = i5 + 1){
-            System.out.println(i5 * i5);
-            System.out.println(i5 + 3);
-            System.out.println(i5);
-        }
-
+        System.out.println("All done");
     }
 }
