39e6690b80a43373148c920d3b9cdea9878c77e9
Sarah-DeMatteis <816154@mystma.org>
2020-04-21 18:33:59 +0000

    4/21 Challenge: Counting to 19 twice

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -3,15 +3,30 @@ package app;
 public class App {
     public static void main(String[] args) throws Exception {
 
-        for (int i1 = 1; i1 <= 20; i1++) {
+        System.out.println("Start first loop");
 
-            if (i1 % 4 == 0) {
-                System.out.println(i1 + " is divisible by 4.");
-            } else {
-                System.out.println(i1 + " is not divisible by 4.");
+        for(int i1 = 0; i1 <= 19; i1++){
+            System.out.println(i1);
+
+            if(i1 == 10){
+
+                for(int i2 = 1; i2 <= 5; i2++){
+                    System.out.println(i1);
+                }
             }
         }
 
-        System.out.println("All done");
+        System.out.println("End first loop");
+        System.out.println("Start second loop");
+
+        for(int i3 = -1; i3 <= 19; i3++){
+
+            if(i3 % 3 == 0){               
+            } else {
+                System.out.println(i3);
+            }
+        }
+
+        System.out.println("End second loop");
     }
 }
