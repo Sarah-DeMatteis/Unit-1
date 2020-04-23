f103e872c5777d51b8e4f49c9e94b5e2a1dc399e
Sarah-DeMatteis <816154@mystma.org>
2020-04-23 16:33:55 +0000

    Digits and junk food- 4/23

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -3,35 +3,26 @@ package app;
 public class App {
     public static void main(String[] args) throws Exception {
 
-        System.out.println("Start first loop");
-
-        for(int i = 20; i >= 1; i = i - 1){
-            System.out.println(i);
-
-            if(i == 10){
-
-                for(int j = 1; j <= 7; j++){
-                    System.out.println(i);
-                }
-            }
-        }
-
-        System.out.println("End first loop");
-        System.out.println("Start second loop");
-
-        for(int i = 0; i <= 38; i = i + 2){
-
-            int j = i - 2;
-
-            if(j % 12 == 0){
-                System.out.println("Hey!");
+        System.out.println("Double or single digit?");
+        
+        for(int i = 20; i >= 1; i = i-1){
+      
+            if(i > 9){
+                System.out.println(i + " is double digit");
             } else {
-                if(i % 4 == 0){
-                    System.out.println(i);
-                }
+                System.out.println(i + " is single digit");
             }
         }
 
-        System.out.println("End second loop");
+        System.out.println("Nice!");
+        System.out.println("Fast food");
+
+        for(int i = 0; i <= 4; i++){
+            System.out.println("Burgers");
+            System.out.println("Fries");
+        }
+
+        System.out.println("Nice!");
+
     }
 }
