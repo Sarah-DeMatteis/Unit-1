a09e30ba18252227f43f58171342f375c78f4eeb
Sarah-DeMatteis <816154@mystma.org>
2020-05-15 15:57:18 +0000

    Dogs

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -7,49 +7,25 @@ public class App {
     public static void main(String[] args) throws Exception {
 
         Scanner input = new Scanner(System.in);
-        int i = 0;
 
-        System.out.println("Free Calculator: Enter two numbers to add, subtract, multiply, or divide.");
+        Dog dog1 = new Dog();
+        dog1.name = "Rufus";
+        dog1.breed = "German Shepherd";
+        dog1.age = 7;
 
-         System.out.println("Please enter a first number.");
-        int num1 = input.nextInt();
+        Dog dog2 = new Dog();
+        dog2.name = "Yasmina";
+        dog2.breed = "Poodle";
+        dog2.age = 2;
 
-        System.out.println("Please enter a second number.");
-        int num2 = input.nextInt();
-
-        while (i == 0) {
-
-            System.out.println("Please select an operation for your numbers.");
-            System.out.println("Type 1 for add, 2 for subtract, 3 for multiply, or 4 for divide");
-            int operation = input.nextInt();
-
-            if(operation == 1){
-                int ans = num1 + num2;
-                System.out.println(ans);
-                i = 1;
-            } else {
-                if(operation == 2){
-                    int ans = num1 - num2;
-                    System.out.println(ans);
-                    i = 1;
-                } else {
-                    if(operation == 3){
-                        int ans = num1 * num2;
-                        System.out.println(ans);
-                        i = 1;
-                    } else {
-                        if(operation == 4){
-                            int ans = num1 / num2;
-                            System.out.println(ans);
-                            i = 1;
-                        } else {
-                            System.out.println("Sorry, your operation was not recognized. Please reenter your operation and try again.");
-                        }
-                    }       
-                }
-            }
-        }
-
-
+        Dog dog3 = new Dog();
+        dog3.name = "Bo";
+        dog3.breed = "Daschund";
+        dog3.age = 5;
+        
+        System.out.println("Dog 1 is a " + dog1.breed + " named " + dog1.name + ". " + dog1.name + " is " + dog1.age + " years old.");
+        System.out.println("Dog 2 is a " + dog2.breed + " named " + dog2.name + ". " + dog2.name + " is " + dog2.age + " years old.");
+        System.out.println("Dog 3 is a " + dog3.breed + " named " + dog3.name + ". " + dog3.name + " is " + dog3.age + " years old.");
+ 
     }
 }
